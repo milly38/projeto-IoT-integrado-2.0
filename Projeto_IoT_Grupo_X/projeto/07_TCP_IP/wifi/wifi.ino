78a3c6e3540d369857166b907cd20f37256ae53f
Emilly de Oliveira <mi.estrela08@gmail.com>
2026-05-08 15:39:24 -0300

    Modify Wi-Fi setup for new credentials and messages
    Updated Wi-Fi credentials and improved connection messages.

--- Projeto_IoT_Grupo_X/projeto/07_TCP_IP/wifi/wifi.ino
+++ Projeto_IoT_Grupo_X/projeto/07_TCP_IP/wifi/wifi.ino
@@ -1,23 +1,37 @@
-#include <ESP8266WiFi.h>
+o professor acabou de mandar esse - #include <ESP8266WiFi.h>
 
-const char* ssid = "programador";
-const char* password = "2306eg";
+// Nome da rede Wi-Fi
+const char* ssid = "CONECTADO";
+
+// Senha da rede Wi-Fi
+const char* password = "C@necTa0!";
 
 void setup() {
-
+  
+  // Inicializa comunicação serial
   Serial.begin(115200);
+  
+  Serial.println();
+  Serial.println("Conectando ao Wi-Fi...");
 
+  // Inicia conexão Wi-Fi
   WiFi.begin(ssid, password);
 
+  // Aguarda conexão
   while (WiFi.status() != WL_CONNECTED) {
-
-    delay(1000);
-    Serial.println("Conectando...");
+    delay(500);
+    Serial.print(".");
   }
 
-  Serial.println("WiFi conectado!");
+  // Exibe conexão realizada
+  Serial.println();
+  Serial.println("Wi-Fi conectado com sucesso!");
+
+  // Exibe IP obtido
+  Serial.print("IP do ESP8266: ");
+  Serial.println(WiFi.localIP());
 }
 
 void loop() {
-
+  
 }
